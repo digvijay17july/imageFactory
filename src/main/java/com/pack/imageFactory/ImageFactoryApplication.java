f8f9f26fb8df89d1a6579e91f6f2c13721d8753b
digvijay17july@gmail.com <digvijay17july@gmail.com>
2019-08-16 16:36:43 +0530

    initial commit

---
+++ src/main/java/com/pack/imageFactory/ImageFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.pack.imageFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFactoryApplication.class, args);
+	}
+
+}
